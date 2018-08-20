5ec29ddbf1112c0c160e9da2d44d319203ab8b7f
DESKTOP-LN1ECH5\cicct <monkeyd.paulbart@gmail.com>
2018-08-20 22:13:49 +0800

    Modify changes commit

---
+++ app/src/main/java/com/elano/spotify/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.elano.spotify
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.SparseBooleanArray
+import android.view.View
+import android.widget.Toast
+import com.elano.spotify.fragment.MusicFragment
+import com.elano.spotify.model.SongInfo
+import com.elano.spotify.adapter.RecyclerItemClickListener
+import com.elano.spotify.adapter.SongAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), RecyclerItemClickListener.ClickListener {
+
+    private lateinit var mSongList: ArrayList<SongInfo>
+    private lateinit var mAdapter: SongAdapter
+    private lateinit var mSongCursor: Cursor
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mSelectedItem: SparseBooleanArray
+
+    companion object {
+        const val SONG_DATA = "key-song-data"
+        const val REQUEST_CODE_PERMISSION = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mSongList = ArrayList()
+        mSelectedItem = SparseBooleanArray()
+        mAdapter = SongAdapter(mSelectedItem, mSongList)
+        mRecyclerView = recyclerView
+        tvHeading.text = getString(R.string.text_heading)
+        setSupportActionBar(toolbar)
+        recyclerViewInit()
+        prepareSongData()
+    }
+
+    private fun recyclerViewInit() {
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.itemAnimator = DefaultItemAnimator()
+        mRecyclerView.adapter = mAdapter
+        mRecyclerView.addOnItemTouchListener(RecyclerItemClickListener(this, this))
+    }
+
+    override fun onItemClick(view: View, position: Int) {
+        val fragmentContainer = fragment; val musicFragment = MusicFragment()
+        val bundle = Bundle(); val songPosition = mSongList[position]
+
+        if (mSelectedItem.get(position, false)) {
+            mSelectedItem.delete(position)
+            view.isSelected = false
+        }
+        else {
+            bundle.putParcelable(SONG_DATA, songPosition)
+            musicFragment.arguments = bundle
+            supportFragmentManager.beginTransaction().replace(R.id.fragment, musicFragment).commit()
+            fragmentContainer.visibility = View.VISIBLE
+            mSelectedItem.put(position, true)
+            view.isSelected = true
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun prepareSongData() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE_PERMISSION)
+        else {
+            mSongCursor = contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, null)
+            while (mSongCursor.moveToNext()) {
+                val title = mSongCursor.getString(mSongCursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
+                val artist = mSongCursor.getString(mSongCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+                val album = mSongCursor.getString(mSongCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+                val path = mSongCursor.getString(mSongCursor.getColumnIndex(MediaStore.Audio.Media.DATA))
+
+                mSongList.add(SongInfo(title, artist, album, path))
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSION && (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            prepareSongData()
+        }
+    }
+}
---
+++ app/src/main/java/com/elano/spotify/fragment/MusicFragment.kt
@@ -0,0 +1,142 @@
+package com.elano.spotify.fragment
+
+import android.graphics.Color
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import com.elano.spotify.MainActivity
+import com.elano.spotify.R
+import com.elano.spotify.model.SongInfo
+import java.io.IOException
+import kotlinx.android.synthetic.main.fragment_music.*
+import kotlinx.android.synthetic.main.fragment_music.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MusicFragment : Fragment(), View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener, SeekBar.OnSeekBarChangeListener {
+
+    private lateinit var mTvSongPlay: TextView
+    private lateinit var mBtnPlay: Button
+    private var mHandler: Handler? = null
+    private var mMediaPlayer: MediaPlayer? = null
+    private var mRunnable: Runnable? = null
+    private var songProgressBar: SeekBar? = null
+
+    companion object {
+        const val UPDATE_FREQUENCY = 100L
+        const val TAG = "MusicFragment"
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_music, container, false)
+        val song = arguments?.getParcelable(MainActivity.SONG_DATA) as SongInfo
+
+        setViews(rootView)
+        mHandler = Handler()
+        mMediaPlayer = MediaPlayer()
+        songProgressBar?.progressDrawable?.setColorFilter(Color.WHITE, android.graphics.PorterDuff.Mode.SRC_IN)
+        songProgressBar?.setPadding(0, 0, 0, 0)
+        songProgressBar?.progress = 0
+        try {
+            if (mMediaPlayer!!.isPlaying)
+                mMediaPlayer?.reset()
+            mMediaPlayer?.setDataSource(song.path)
+            mMediaPlayer?.prepare()
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        mTvSongPlay.isSelected = true
+        mTvSongPlay.text = ("${song.name} ${getString(R.string.text_bullet)} ${song.singer}")
+        mBtnPlay.setOnClickListener(this)
+        mMediaPlayer?.setOnPreparedListener(this)
+        mMediaPlayer?.setOnCompletionListener(this)
+        songProgressBar?.setOnSeekBarChangeListener(this)
+
+        return rootView
+    }
+
+    private fun updateProgressBar() {
+        songProgressBar?.progress = mMediaPlayer!!.currentPosition
+        if (mMediaPlayer!!.isPlaying) {
+            mRunnable = Runnable {
+                updateProgressBar()
+            }
+        }
+        mHandler?.postDelayed(mRunnable, UPDATE_FREQUENCY)
+    }
+
+    override fun onClick(view: View?) {
+        if (mMediaPlayer!!.isPlaying) {
+            mMediaPlayer?.pause()
+            setPlayImage(view)
+        }
+        else {
+            mMediaPlayer?.seekTo(mMediaPlayer!!.currentPosition)
+            mMediaPlayer?.start()
+            setPauseImage(view)
+        }
+        songProgressBar?.progress = mMediaPlayer!!.currentPosition
+    }
+
+    override fun onPrepared(mediaPlayer: MediaPlayer?) {
+        mMediaPlayer?.start()
+        setPauseImage(btnPlayPause)
+        songProgressBar?.max = mMediaPlayer!!.duration
+        updateProgressBar()
+    }
+
+    override fun onCompletion(mediaPlayer: MediaPlayer?) {
+        setPlayImage(btnPlayPause)
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        if (fromUser)
+            mMediaPlayer?.seekTo(progress)
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        Log.e(TAG, "Progress: ${seekBar?.progress}")
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        Log.e(TAG, "Progress: ${seekBar?.progress}")
+    }
+
+    private fun setPlayImage(view: View?) {
+        view?.setBackgroundResource(R.drawable.ic_play)
+    }
+
+    private fun setPauseImage(view: View?) {
+        view?.setBackgroundResource(R.drawable.ic_pause)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mHandler?.removeCallbacks(mRunnable)
+        mMediaPlayer?.stop()
+        mMediaPlayer?.release()
+        mMediaPlayer = null
+    }
+
+    private fun setViews(view: View) {
+        mTvSongPlay = view.tvSongPlay
+        mBtnPlay = view.btnPlayPause
+        songProgressBar = view.seekBar
+    }
+
+}// Required empty public constructor
